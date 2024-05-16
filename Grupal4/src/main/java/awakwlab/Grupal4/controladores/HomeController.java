6e3ba4a9492c03cf1c2f45d57d3ff6fe83030db9
ShahrazadM <shahrazadlksmi@gmail.com>
2024-05-16 01:15:19 -0400

    Grupal 4.1

--- Grupal4/src/main/java/awakwlab/Grupal4/controladores/HomeController.java
+++ Grupal4/src/main/java/awakwlab/Grupal4/controladores/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public ModelAndView mostrarHome() {
         ModelAndView modelAndView = new ModelAndView("inicio"); // Cambiado a "inicio"
-        modelAndView.addObject("mensaje", "Bienvenido al Portal xD");
+        modelAndView.addObject("mensaje", "Bienvenido al Portal.....");
         return modelAndView;
     }
 }
